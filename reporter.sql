b24c85c304af3aa33d5698e206de8b597ea44dd2
Jaredible <jareddiehl96@gmail.com>
2020-04-05 21:09:34 -0500

    Update reporter.sql

--- reporter.sql
+++ reporter.sql
@@ -3,13 +3,13 @@ CREATE DATABASE IF NOT EXISTS reporter;
 USE reporter;
 
 CREATE TABLE IF NOT EXISTS assignment (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title varchar(20) NOT NULL,
   author varchar(20) NOT NULL,
   due_date timestamp NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS question (
-  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   content varchar(500) NOT NULL
 );
